e986cdc634e713d7c52a0a91ee250a9b764c5bf9
naor yanko <na0ryank0@gmail.com>
2026-01-05 13:31:57 +0000

    File changes

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useSearchParams, useNavigate } from 'react-router-dom';
-import { Property } from "@/entities/Property";
 import { ChatQuestion } from "@/entities/ChatQuestion";
 import { ChatSession } from "@/entities/ChatSession";
 import PropertyResults from '../components/properties/PropertyResults';
