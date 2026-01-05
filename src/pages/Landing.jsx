b1c7e3219b04f32913c44aa6c6d068209ed8b907
naor yanko <na0ryank0@gmail.com>
2026-01-05 12:24:05 +0000

    File changes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -207,7 +207,7 @@ export default function Landing() {
         </div>
 
         {/* Scroll indicator */}
-        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
+        <div className="absolute bottom-32 left-1/2 transform -translate-x-1/2">
           <ChevronDown className="w-8 h-8 text-slate-700/80 animate-bounce" />
         </div>
       </div>
